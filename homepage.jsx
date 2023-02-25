8ec7241a79605bb98bb67ec4fd07b372501b8782
Shiv13527 <shivpratappundir97@gmail.com>
2023-02-25 22:50:40 +0530

    Create homepage.jsx

---
+++ homepage.jsx
@@ -0,0 +1,11 @@
+import { Col, Container, Row, Spinner} from 'react-bootstrap';
+import HomeAside from '../components/HomeAside';
+import Timeline from '../components/Timeline';
+
+
+const Home = () => {
+    
+
+};
+
+export default Home;
